be45972b29dd9a1c97ec7feae742025e7f95964e
Michael <michael.ungar03@gmail.com>
2025-12-11 00:10:28 +0100

    feat: add listener that checks media query for theming

--- src/darkmode.ts
+++ src/darkmode.ts
@@ -4,3 +4,9 @@ document.documentElement.classList.toggle(
     (!("theme" in localStorage) &&
       window.matchMedia("(prefers-color-scheme: dark)").matches)
 );
+
+window.matchMedia("(prefers-color-scheme: dark)").addEventListener("change", (e) => {
+  if (!("theme" in localStorage)) {
+    document.documentElement.classList.toggle("dark", e.matches);
+  }
+});
